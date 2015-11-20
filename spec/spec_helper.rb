2e8dd733a5cbc5b97d6c74e525dfc7bb2a6b54d6
joshuabamboo <joshuaowens011@gmail.com>
2015-11-19 20:57:43 -0500

    initial spec commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+ENV["SCHOOL_ENV"] = "test"
+
+require_relative '../config/environment'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.order = 'default'
+
+  config.before do
+    reset_database
+  end
+end
+
+def reset_database
+  migrate_db
+end
+
+def clean_database
+  Student.delete_all if defined?(Student) && DB.tables.include?("students")
+end
