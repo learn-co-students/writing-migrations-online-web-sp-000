37f63cd639d0e6ad0b13b7d55879a63d73750fc9
curiositypaths <curiositypaths@users.noreply.github.com>
2018-02-26 12:05:47 -0500

    Add ActiveRecord::Migration version

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
     change_column :students, :birthdate, :datetime
   end
