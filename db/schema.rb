0f9e3b5cd038536b8e9ff8caec8a6391f4e744d3
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-13 19:33:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
-    t.string "name"
+    t.integer "grade"
+    t.datetime "birthday"
   end
 
 end
