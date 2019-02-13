886c85c904728895c74517af74281231319acc5a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-13 19:33:09 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change_column
-    change_column :table_name, :column_name, :type
+    change_column (:students, :birthdate, :datetime)
   end
 end
