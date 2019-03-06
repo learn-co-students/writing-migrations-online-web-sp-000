76f62c99988010c01cae5440e000c34569214d7e
Tom Holmes <tholmes59@gmail.com>
2019-03-06 00:34:28 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[4.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
