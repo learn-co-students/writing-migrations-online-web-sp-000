f560a2d55ae1a4e7fccb6ebbbfa813b52f7438d0
Meisuleen Kased <meisuleen@gmail.com>
2019-04-06 01:43:52 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,10 @@
 class  AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column(:grade, :birthdate)
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
+
+  # def change
+  #   add_column :students, :birthdate, :string
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
+    t.string "birthdate"
   end
 
 end
