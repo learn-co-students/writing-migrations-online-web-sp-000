da8c37fc2b77511fc6d69ffcd6f31be85f59336a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-30 00:22:27 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -3,7 +3,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
     add_column :students, :grade, :integer
-    add_column :students, :brithdate, :string
+    add_column :students, :birthdate, :string
   end
 
 
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -10,4 +10,5 @@ class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
 
 
+
 end
