d9909684a3b9c2d428883c796af1493c09762956
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-05 22:15:25 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "grade"
-    t.string "birthdate"
+    t.datetime "birthdate"
   end
 
 end
