ce7205c9969ea07befdf345155948ff5c4619c7b
Anthony Tilelli <anthony@tilelli.me>
2020-01-11 04:38:00 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,1 +1,5 @@
-class ChangeDatatypeForBirthdate <
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+    def change
+      change_column :students, :birthdate, :datetime
+    end
+end
