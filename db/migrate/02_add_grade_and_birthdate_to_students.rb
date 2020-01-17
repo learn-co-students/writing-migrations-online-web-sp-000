0376ff56bf83dc9109d23499249c607220950c3b
yaoyu bao <yaobao5354@gmail.com>
2020-01-17 20:37:38 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,10 @@
+require 'pry'
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+
+  end
+
+end
