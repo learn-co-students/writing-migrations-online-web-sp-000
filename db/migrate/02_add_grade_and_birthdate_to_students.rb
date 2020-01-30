16e88adde114d67b7950cd4947d5943d50231b39
Shayla White <shayladwhite1@gmail.com>
2020-01-30 18:25:26 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,7 @@
- 
- 
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 end
+
