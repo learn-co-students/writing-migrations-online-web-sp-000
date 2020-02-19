08581465a738425255341d60e60491b45aaaffd6
LIManuel <leonmalisov@gmail.com>
2020-02-19 17:58:34 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "grade"
-    t.string "birthdate"
+    t.datetime "birthdate"
   end
 
 end
