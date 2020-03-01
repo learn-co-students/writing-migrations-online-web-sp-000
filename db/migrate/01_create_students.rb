756861487d4f0ce8b0566b52e5ab92d4ca019226
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-01 14:00:40 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :students do |s|
+      s.string :name
+      #s.string :genre
+      #s.integer :age
+      #s.string :hometown
+    end
+  end
 end
