1a523661f3a884f3fb5b3ecf7c6ea97655d8d99a
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-04-24 10:31:53 -0400

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,12 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-    def change
+    # def change
+    #     change_column :students, :birthdate, :datetime
+    # end
+    def up
         change_column :students, :birthdate, :datetime
+    end
+
+    def down
+        change_column :students, :birthdate, :string
     end
 end
