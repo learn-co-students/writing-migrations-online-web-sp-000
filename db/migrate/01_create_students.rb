48e791a99c575d8dd74d8fd555fcef1c2bb0ffa8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-18 21:45:09 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
-end
+class CreateStudents < ActiveRecord::Migration[5.2]
+
+    def change
+      create_table :students do |t|
+        t.string :name
+      end
+    end
+  end
+
+# Active Record create table method within change method
+# name column with type string
+# rake db:migrate to run the migrations
