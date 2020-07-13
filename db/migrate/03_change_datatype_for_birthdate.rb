9efd893fa4ad3e91a08bdadad555d0c624ae73dd
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-13 05:26:48 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    change_column(:students, :birthdate, :datetime)
+    change_column :students, :birthdate, :datetime
   end
 end
