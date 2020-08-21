5f426e4ada3227d0b74b054cddfe005f12534fbd
sparsell <hello@sparsell.com>
2020-08-21 07:05:03 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,8 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2]
+
+    def change
+        create_table :students do |t|
+            t.string :name
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
