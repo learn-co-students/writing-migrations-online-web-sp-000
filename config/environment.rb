945623b8c0a103fb6b0be2300588207068157348
anpham148 <anpham148@gmail.com>
2020-09-11 14:40:03 +0100

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,10 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/students.sqlite"
+)
+ 
+require_relative "../app/models/student.rb"
