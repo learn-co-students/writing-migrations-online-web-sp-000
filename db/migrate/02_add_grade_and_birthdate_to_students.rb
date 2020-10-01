bd178dcd7d036ac60b42a524e2dd2ae3ba8850f7
bczorn <bczorn@gmail.com>
2020-09-30 21:30:18 -0500

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,8 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        add_column :students, :grade, :integer
+        add_column :students, :birthdate, :string
+    end
+
+end
