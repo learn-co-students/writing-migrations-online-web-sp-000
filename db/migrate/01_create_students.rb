24eb20624cafe35f89a0005c3c9803866a8293e9
github username <github email address>
2020-10-09 14:52:20 -0700

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,8 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[4.2]
+   def change
+    create_table :students do |x|
+        x.string :name
+   end
+ 
+    end
 end
