32ffa6bf541b12af389276190403216807bd53d9
Alan Tang <giabao_tang@yahoo.com>
2020-12-01 21:04:06 -0800

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 ENV["SCHOOL_ENV"] = "test"
 
 require_relative '../config/environment'
-
+require_relative '../db/migrate/02_add_grade_and_birthdate_to_students'
+require_relative '../db/migrate/03_change_datatype_for_birthdate'
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.order = 'default'
