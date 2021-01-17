7c12b98a4efaa67896967bd1dae5ee290e812a99
Sean Flanagan <sean.nycity@gmail.com>
2021-01-17 23:05:25 +0000

    Done.

--- lib/support/db_registry.rb
+++
@@ -1,6 +0,0 @@
-require 'ostruct'
-require_relative './connection_adapter'
-DBRegistry ||= OpenStruct.new(test: ConnectionAdapter.new("db/school-test.db"), 
-  development: ConnectionAdapter.new("db/school-development.db"), 
-  production: ConnectionAdapter.new("db/school-production.db")
-)
